da8d27d728ffff9d436437dd4a350b236732255f
MoQuant <112667516+MoQuant@users.noreply.github.com>
2024-11-09 17:41:30 -0500

    Update intro_to_neural_networks_2.py

--- intro_to_neural_networks_2.py
+++ intro_to_neural_networks_2.py
@@ -1,124 +1,133 @@
 import numpy as np
 
+# Neural Network Class
+class NeuralNetwork:
 
-class NNET:
+    def __init__(self, iN, oN, epochs=500):
+        self.M = iN
+        self.N = oN
+        self.epochs = list(range(epochs))
 
-    def __init__(self, inx, outx, epochs=100):
-        self.m = inx
-        self.n = outx
-        self.epochs = range(epochs)
-
-        self.minx = {}
-        self.maxx = {}
-        self.miny = {}
-        self.maxy = {}
-
+    # Trains data
     def __call__(self, x, y):
         bias = -1
         for epoch in self.epochs:
             # Forward propigation
-            for step in self.axis:
-                if step == self.axis[0]:
-                    self.L[step] = x @ self.W[step] + bias
-                    self.SL[step] = self.sigmoid(self.L[step])
+            for i in self.axis:
+                if i == self.axis[0]:
+                    self.L[i] = x @ self.W[i] + bias
                 else:
-                    self.L[step] = self.SL[step+1] @ self.W[step] + bias
-                    self.SL[step] = self.sigmoid(self.L[step])
+                    self.L[i] = self.SL[i+1] @ self.W[i] + bias
+                self.SL[i] = self.sigmoid(self.L[i])
 
-            # Backpropigation
-            update = {}
-            for step in self.raxis:
-                if step == self.raxis[0]:
-                    error = (y - self.SL[step])**2
-                    delta = 2*(y - self.SL[step])*self.sigmoid(self.L[step], derv=True)
-                    update[step] = delta
+            # Back Propigation
+            delta = {}
+            for i in self.baxis:
+                if i == self.baxis[0]:
+                    error = (y - self.SL[i])**2
+                    delta[i] = 2.0*(y - self.SL[i])*self.sigmoid(self.L[i], derv=True)
                 else:
-                    error = self.W[step-1] @ delta
-                    delta = error*self.sigmoid(self.L[step], derv=True)
-                    update[step] = delta
+                    error = self.W[i-1] @ delta[i-1]
+                    delta[i] = error*self.sigmoid(self.L[i], derv=True)
 
             for i in self.axis:
-                self.W[i] += update[i]
+                self.W[i] += delta[i]
 
-    def testmodel(self, x):
+    # Test output
+    def testData(self, x):
         bias = -1
-        for step in self.axis:
-            if step == self.axis[0]:
-                self.L[step] = x @ self.W[step] + bias
-                self.SL[step] = self.sigmoid(self.L[step])
+        for i in self.axis:
+            if i == self.axis[0]:
+                self.L[i] = x @ self.W[i] + bias
             else:
-                self.L[step] = self.SL[step+1] @ self.W[step] + bias
-                self.SL[step] = self.sigmoid(self.L[step])
+                self.L[i] = self.SL[i+1] @ self.W[i] + bias
+            self.SL[i] = self.sigmoid(self.L[i])
         return self.SL[self.axis[-1]]
 
-    def normalization(self, x, y):
-        m, n = x.shape
-        for i in range(n):
-            self.minx[i] = min(x[:, i])
-            self.maxx[i] = max(x[:, i])
-        o, p = y.shape
-        for i in range(p):
-            self.miny[i] = min(y[:, i])
-            self.maxy[i] = max(y[:, i])
-        for i in range(n):
-            x[:, i] = (x[:, i] - self.minx[i])/(self.maxx[i] - self.minx[i])
-        for i in range(p):
-            y[:, i] = (y[:, i] - self.miny[i])/(self.maxy[i] - self.miny[i])
-        return x, y
-
-    def convert(self, y):
-        o, p = y.shape
-        for i in range(p):
-            y[:, i] = y[:, i]*(self.maxy[i] - self.miny[i]) + self.miny[i]
-        return y
-
-    def build(self):
-        self.axis = list(range(self.m, self.n, -1))
-        self.raxis = self.axis[::-1]
-
-        self.W = {}
-        self.L = {}
-        self.SL = {}
-
-        for i in self.axis:
-            self.W[i] = np.random.rand(i, i-1)
-            self.L[i] = np.zeros(i-1)
-            self.SL[i] = np.zeros(i-1)
-
+    # Activation and optimization function
     def sigmoid(self, x, derv=False):
         f = 1.0 / (1.0 + np.exp(-x))
         if derv:
             return f*(1 - f)
         return f
 
-    def splitter(self, X, Y, prop=0.7):
-        n = int(prop*len(X))
-        tX = X[:n]
-        tY = Y[:n]
-        eX = X[n:]
-        eY = Y[n:]
-        return tX, tY, eX, eY
+    # Builds parameters
+    def buildWeights(self):
+        self.axis = list(range(self.M, self.N, -1))
+        self.baxis = self.axis[::-1]
+
+        self.W = {i:np.random.rand(i, i-1) for i in self.axis}
+        self.L = {i:np.zeros(i-1) for i in self.axis}
+        self.SL = {i:np.zeros(i-1) for i in self.axis}
+
+    # Split data into training and testing
+    def trainTestSplit(self, x, y, proportion=0.7):
+        I = int(proportion*len(x))
+        trainX = x[:I]
+        trainY = y[:I]
+        testX = x[I:]
+        testY = y[I:]
+        return trainX, trainY, testX, testY
+    
+    # Normalize the data via min/max method
+    def normalizeData(self, x, y):
+        x, y = np.array(x), np.array(y)
+        self.lowx = {}
+        self.highx = {}
+        self.lowy = {}
+        self.highy = {}
+        m, n = self.M, self.N
+        for i in range(m):
+            self.lowx[i] = np.min(x[:, i])
+            self.highx[i] = np.max(x[:, i])
+            x[:, i] = (x[:, i] - self.lowx[i])/(self.highx[i] - self.lowx[i])
+        for i in range(n):
+            self.lowy[i] = np.min(y[:, i])
+            self.highy[i] = np.max(y[:, i])
+            y[:, i] = (y[:, i] - self.lowy[i])/(self.highy[i] - self.lowy[i])
+        return x, y
+    
+    # Convert normalized data back to original scale
+    def denomalizeData(self, y):
+        y = np.array(y)
+        m, n = y.shape
+        for i in range(n):
+            y[:, i] = y[:, i]*(self.highy[i] - self.lowy[i]) + self.lowy[i]
+        return y
+
         
 
+rows = 400
+X = 100*np.random.rand(rows, 15)
+Y = 10*np.random.rand(rows, 3)
 
-net = NNET(6, 2)
+AI = NeuralNetwork(15, 3, epochs=50)
+AI.buildWeights()
 
-X = (100*np.random.rand(50, 6))
-Y = (100*np.random.rand(50, 2))
+trainX, trainY, testX, testY = AI.trainTestSplit(X, Y)
 
-nX, nY = net.normalization(X, Y)
+nTrainX, nTrainY = AI.normalizeData(trainX.tolist(), trainY.tolist())
+nTestX, nTestY = AI.normalizeData(testX.tolist(), testY.tolist())
 
-trainX, trainY, testX, testY = net.splitter(nX, nY)
+for inx, iny in zip(nTrainX, nTrainY):
+    AI(inx, iny)
 
-net.build()
-
-for ix, iy in zip(trainX, trainY):
-    net(ix, iy)
-
-yhat = net.convert(net.testmodel(testX))
-testY = net.convert(testY)
-
-for y0, yh in zip(testY, yhat):
-    print(y0, yh, y0 - yh)
+Ypred = []
+for inx in nTestX:
+    iny = AI.testData(inx).tolist()
+    Ypred.append(iny)
     
+YHat = AI.denomalizeData(np.array(Ypred))
 
+RMSE = 0
+for actual, prediction in zip(testY, YHat):
+    RMSE += np.sum(pow(actual - prediction, 2))
+
+RMSE = np.sqrt(RMSE/(len(YHat) - 1))
+
+print("Model Accuracy: ", RMSE)
+
+#nYhat = AI.testData(nTestX)
+#Yhat = AI.denomalizeData(nYhat.tolist())
+
+#print(testY)
